514a827ca1f582bea067a88292f27c3f13c2bf57
Jhony de Oliveira Rodrigues <jhony.o_rodrigues@hotmail.com>
2021-05-24 16:47:29 -0300

    configuração do setup inicial.

---
+++ src/main/java/br/com/bootcamp/TemplateMercadoLivreApplication.java
@@ -0,0 +1,13 @@
+package br.com.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateMercadoLivreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateMercadoLivreApplication.class, args);
+	}
+
+}
